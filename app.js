245b4e8a039932bb77eb1012215a9c3aee9344b6
unknown <dejanmaric259@gmail.com>
2024-06-30 11:54:23 +0200

    building general player and quest logic

--- app.js
+++ app.js
@@ -15,6 +15,38 @@
 //
 const gameContainer = document.getElementById("game-container");
 const continueButton = document.getElementById("continue-button");
+const actionsContainer = document.querySelector(".actions-container");
+const playerLevel = document.getElementById("player-level");
+
+const SteelDager = {
+  type: "Weapon",
+  name: "Steel Dagger",
+  damage: 50,
+  rarity: "Common",
+};
+
+const Player = {
+  playerName: "Mysterios Warrior",
+  health: 100,
+  level: 1,
+  xp: 0,
+  goalXp: 100,
+  inventory: {
+    weapon: SteelDager,
+    healing: "Health Potion",
+  },
+
+  //player methods
+  getPlayerName: (name) => {
+    console.log(name);
+  },
+  checkXp: (currentXp, goalXp) => {
+    if (currentXp >= goalXp) {
+      console.log("Level Up!");
+    }
+  },
+  // levelUp: (newLevel) => {},
+};
 
 const quests = [
   {
@@ -24,22 +56,35 @@ const quests = [
       "As you begin your journey, you come across the Village of Hope under attack by bandits. The villagers plead for your help. Defeating the bandits will earn you the villagers gratitude and valuable information about Malakars whereabouts.",
     questObjective: "Help the villagers of Hope fend off a bandit attack.",
     completed: false,
+    action: "Help villagers",
+    questActionFunction: () => {
+      const helpAttackBandit = document.createElement("button");
+      helpAttackBandit.innerText = `Attack with ${Player.inventory.weapon.name}`;
+      actionsContainer.appendChild(helpAttackBandit);
+      helpAttackBandit.onclick = () => {
+        Player.health = Player.health - 50;
+        Player.xp = Player.xp + 100;
+      };
+    },
   },
 ];
-const Player = {
-  playerName: "Mysterios Warrior",
-  getPlayerName: (name) => {
-    console.log(name);
-  },
-};
 
-continueButton.addEventListener("click", () => {
-  startQuest(0);
-});
+const initilizeGame = () => {
+  playerLevel.innerText = `Level ${Player.level}`;
+};
+initilizeGame();
 
 const startQuest = (index) => {
   const questSummary = quests[index].questSummary;
   const newPar = document.createElement("p");
   newPar.innerText = questSummary;
   gameContainer.appendChild(newPar);
+  continueButton.classList.toggle("hidden");
+  const newAction = document.createElement("button");
+  newAction.innerText = quests[index].action;
+  actionsContainer.appendChild(newAction);
+  newAction.onclick = () => {
+    newAction.classList.add("hidden");
+    quests[index].questActionFunction();
+  };
 };
